839ad1228cdf1dff5c5186021cb51888cd4f5d30
IlPytone <vqslevqsle@gmail.com>
2019-05-15 19:22:27 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,16 @@
 import re
 import telepot
 import requests
+import pornhub
 from flask import Flask, request
 
 try:
     from Queue import Queue
 except ImportError:
     from queue import Queue
-
-coffe_link_mcd = "https://survey.fast-insight.com/mcd/it/voucher_coffee_aug.php"
-headers = {
-	"lang" : "it",
-	"store_id" : "0280",
-	"surveyform" : "1224",
-	"identifier" : "20190429170424",
-	"promo" : "OXM995T9CK4IR",
-	"incentivetype" : "2",
-	"orderpoint" : "1",
-}
 TOKEN = "885925187:AAH9GMyKo6EICdqKc5hzHqzXj2Qxyj_PPMQ"
+search_keywords = []
+total = []
 app = Flask(__name__)
 update_queue = Queue()
 bot = telepot.Bot(TOKEN)
@@ -31,12 +23,17 @@ def on_chat_message(msg):
 			requests.post("https://api.telegram.org/bot885925187:AAH9GMyKo6EICdqKc5hzHqzXj2Qxyj_PPMQ/sendMessage?chat_id="+str(chat_id)+"&text=Buongiorno "+ msg["from"]["first_name"]+", ti auguro di gustarti un buon caffè gratis. 🤙")
 		elif text.startswith("/ping"):
 			requests.post("https://api.telegram.org/bot885925187:AAH9GMyKo6EICdqKc5hzHqzXj2Qxyj_PPMQ/sendMessage?chat_id="+str(chat_id)+"&text=Pong.")
-		elif text.startswith("/coffe"):
-			r = requests.post(coffe_link_mcd, verify = True, data = headers)
-			urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', r.text)
-			link = "http://survey.fast-insight.com/mcd/it/v2025_coupon.php?code=" + str(urls[1])[74:106] + "&type=c"
-			print(link)
-			requests.get("https://api.telegram.org/bot885925187:AAH9GMyKo6EICdqKc5hzHqzXj2Qxyj_PPMQ/sendDocument",verify = True, data = {"chat_id" : chat_id, "document": link, "reply_to_message_id" : msg["message_id"]})
+		elif text.startswith("/ph/"):
+			cat = text[4:]
+			search_keywords.append(cat)
+			client = pornhub.PornHub(search_keywords)
+			for video in client.getVideos(10,page=4):
+				total.append(video)
+			for i in total:
+				requests.get("https://api.telegram.org/bot885925187:AAH9GMyKo6EICdqKc5hzHqzXj2Qxyj_PPMQ/sendPhoto",verify = True, data = {"chat_id" : chat_id, "photo" : i["background"], "caption":"""Title:{0}
+				Url: {1}
+				Duration: {2}
+				Rating: {3}""".format(i["name"], i["url"], i["duration"], i["rating"]), "reply_to_message_id" : msg["message_id"]})
 bot.message_loop({'chat': on_chat_message}, source=update_queue)
 @app.route('/', methods=['GET', 'POST'])
 def pass_update():
